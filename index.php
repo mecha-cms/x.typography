eda2787a8564e75c87478857fc370e16d4ceb9a8
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:36:13 +0700

    Update

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php
 
 namespace x\typography {
+    function content($content) {
+        if (!$content) {
+            return $content;
+        }
+        // Skip parsing process if we are in these HTML element(s)
+        $parts = (array) \preg_split('/(<!--[\s\S]*?-->|' . \implode('|', (static function ($parts) {
+            foreach ($parts as $k => &$v) {
+                $v = '<' . \x($k) . '(?:\s[\p{L}\p{N}_:-]+(?:=(?:"[^"]*"|\'[^\']*\'|[^\/>]*))?)*>(?:(?R)|[\s\S])*?<\/' . \x($k) . '>';
+            }
+            unset($v);
+            return $parts;
+        })([
+            'pre' => 1,
+            'code' => 1, // Must come after `pre`
+            'kbd' => 1,
+            'math' => 1,
+            'script' => 1,
+            'style' => 1,
+            'textarea' => 1
+        ])) . '|https?:\/\/\S+)/', $content, -1, \PREG_SPLIT_NO_EMPTY | \PREG_SPLIT_DELIM_CAPTURE);
+        $content = "";
+        foreach ($parts as $part) {
+            if (0 === \strpos($part, 'http://') || 0 === \strpos($part, 'https://')) {
+                $content .= $part; // Is an URL, skip!
+                continue;
+            }
+            if ($part && '<' === $part[0] && '>' === \substr($part, -1)) {
+                if (false !== \strpos($part, '=')) {
+                    $part = \preg_replace_callback('/<([\p{L}\p{N}_:-]+)(\s(?:"[^"]*"|\'[^\']*\'|[^>])*)?>/', static function ($m) {
+                        return '<' . $m[1] . \preg_replace_callback('/(\s+)(aria-(?:description|label)|alt|summary|title)=(["\'])(.*?)\3/i', static function ($m) {
+                            return $m[1] . $m[2] . '=' . $m[3] . \x\typography\from($m[4], false) . $m[3];
+                        }, $m[2] ?? "") . '>';
+                    }, $part);
+                }
+                $content .= $part; // Is a HTML tag or comment, skip!
+                continue;
+            }
+            $content .= \x\typography\from($part, true);
+        }
+        return "" !== $content ? $content : null;
+    }
     function from(string $content, $n) {
         $dash = ['–', '—'];
         $dot = ['…'];
@@ -58,55 +99,11 @@ namespace x\typography {
         }
         return $content;
     }
-}
-
-namespace x {
-    function typography($content) {
-        if (!$content) {
-            return $content;
-        }
-        // Skip parsing process if we are in these HTML element(s)
-        $parts = (array) \preg_split('/(<!--[\s\S]*?-->|' . \implode('|', (static function ($tags) {
-            foreach ($tags as $k => &$tag) {
-                $tag = '<' . \x($k) . '(?:\s[\p{L}\p{N}_:-]+(?:=(?:"[^"]*"|\'[^\']*\'|[^\/>]*))?)*>(?:(?R)|[\s\S])*?<\/' . \x($k) . '>';
-            }
-            unset($tag);
-            return $tags;
-        })([
-            'pre' => 1,
-            'code' => 1, // Must come after `pre`
-            'kbd' => 1,
-            'math' => 1,
-            'script' => 1,
-            'style' => 1,
-            'textarea' => 1
-        ])) . '|https?:\/\/\S+)/', $content, -1, \PREG_SPLIT_NO_EMPTY | \PREG_SPLIT_DELIM_CAPTURE);
-        $content = "";
-        foreach ($parts as $part) {
-            if (0 === \strpos($part, 'http://') || 0 === \strpos($part, 'https://')) {
-                $content .= $part; // Is an URL, skip!
-                continue;
-            }
-            if ($part && '<' === $part[0] && '>' === \substr($part, -1)) {
-                if (false !== \strpos($part, '=')) {
-                    $part = \preg_replace_callback('/<([\p{L}\p{N}_:-]+)(\s(?:"[^"]*"|\'[^\']*\'|[^>])*)?>/', static function ($m) {
-                        return '<' . $m[1] . \preg_replace_callback('/(\s+)(aria-(?:description|label)|alt|summary|title)=(["\'])(.*?)\3/i', static function ($m) {
-                            return $m[1] . $m[2] . '=' . $m[3] . \x\typography\from($m[4], false) . $m[3];
-                        }, $m[2] ?? "") . '>';
-                    }, $part);
-                }
-                $content .= $part; // Is a HTML tag or comment, skip!
-                continue;
-            }
-            $content .= \x\typography\from($part, true);
-        }
-        return "" !== $content ? $content : null;
-    }
     \Hook::set([
         'page.content',
         'page.description',
         'page.title',
-    ], __NAMESPACE__ . "\\typography", 2.1);
+    ], __NAMESPACE__ . "\\content", 2.1);
 }
 
 namespace {
--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars($content) . '</pre>';
-echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . strtr(htmlspecialchars(x\typography($content)), [
+echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . strtr(htmlspecialchars(x\typography\content($content)), [
     '×' => '<mark style="background:rgba(0,0,0,.15);color:inherit;">×</mark>',
     '–' => '<mark style="background:rgba(0,0,0,.15);color:inherit;">–</mark>',
     '—' => '<mark style="background:rgba(0,0,0,.15);color:inherit;">—</mark>',
